401c9cb9298914be8d23eec2351eda4db0545a90
Ali Khajezade <30939214+xajezade@users.noreply.github.com>
2024-11-28 21:06:50 -0800

    Update restapis.py for linting

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -13,10 +13,10 @@ sentiment_analyzer_url = os.getenv(
 
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs ):
+    if (kwargs):
         for key, value in kwargs.items():
             params = params + key + "=" + value + "&"
-    request_url = backend_url + endpoint + "?"+ params
+    request_url = backend_url + endpoint + "?" + params
     print("GET from {} ".format(request_url))
     try:
         # Call get method of requests library with URL and parameters
