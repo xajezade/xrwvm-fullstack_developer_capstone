ae371e401ba75191c841b2c6ac23095eda839b93
Ali Khajezade <30939214+xajezade@users.noreply.github.com>
2024-11-28 21:09:56 -0800

    Update urls.py for linting

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,14 +13,17 @@ urlpatterns = [
     # path for logout
     path(route='logout', view=views.logout_request, name='logout'),
     # path for get_cars
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    path(route='get_cars', view=views.get_cars, name='getcars'),
     # paths for get_dealerships
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),    
+    path(route='get_dealers/<str:state>',
+         view=views.get_dealerships, name='get_dealers_by_state'),
     # path for dealer detail by ID
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path(route='dealer/<int:dealer_id>',
+         view=views.get_dealer_details, name='dealer_details'),
     # path for dealer reviews view
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path(route='reviews/dealer/<int:dealer_id>',
+         view=views.get_dealer_reviews, name='dealer_details'),
     # path for add a review view
     path(route='add_review', view=views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
