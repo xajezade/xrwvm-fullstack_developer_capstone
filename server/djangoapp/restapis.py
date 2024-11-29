d28ec7da4e0fbb83f057f6973ca170eb07932427
xajezade <ali.xajezade@gmail.com>
2024-11-28 19:51:13 -0500

    completed main app development with dealership listing and review

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -26,7 +26,7 @@ def get_request(endpoint, **kwargs):
         print("Network exception occurred")   
 
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"analyze/"+text
+    request_url = sentiment_analyzer_url+"/analyze/"+text
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
