49c4c2f3091c05835b8d5486aa562e888e9ccbad
Ali Khajezade <30939214+xajezade@users.noreply.github.com>
2024-11-28 21:55:56 -0800

    Update settings.py for linting

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -27,9 +27,10 @@ SECRET_KEY =\
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = ['localhost', 'https://xajezade-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://xajezade-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+myhost = 'https://xajezade-8000.theiadockernext-0-labs-' +
+'prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+ALLOWED_HOSTS = ['localhost', myhost]
+CSRF_TRUSTED_ORIGINS = [myhost]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -62,9 +63,9 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR,'frontend/static'),
-            os.path.join(BASE_DIR,'frontend/build'),
-            os.path.join(BASE_DIR,'frontend/build/static'),
+            os.path.join(BASE_DIR, 'frontend/static'),
+            os.path.join(BASE_DIR, 'frontend/build'),
+            os.path.join(BASE_DIR, 'frontend/build/static'),
             ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -94,7 +95,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.' + 
+        'auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME':
@@ -139,9 +141,9 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'frontend/static'),
-    os.path.join(BASE_DIR,'frontend/build'),
-    os.path.join(BASE_DIR,'frontend/build/static'),
+    os.path.join(BASE_DIR, 'frontend/static'),
+    os.path.join(BASE_DIR, 'frontend/build'),
+    os.path.join(BASE_DIR, 'frontend/build/static'),
     ]
 
 LOGOUT_REDIRECT_URL = '/admin/login/'
